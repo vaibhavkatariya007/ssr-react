a670899572c9572fa2a2c7e13c9b2e80b7e8d624
vaibhavAccionLabs <vaibhav.katariya@accionlabs.com>
2020-05-18 21:44:06 +0800

    push build

---
+++ build/precache-manifest.d6bd082fbcf81e2dba48e81860dc0dd5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "a05b7e4633206cdba51a",
+    "url": "./static/js/main.f13a7835.chunk.js"
+  },
+  {
+    "revision": "7d40aee1a912c4b65f3d",
+    "url": "./static/js/2.ab9e45f7.chunk.js"
+  },
+  {
+    "revision": "a05b7e4633206cdba51a",
+    "url": "./static/css/main.033eef70.chunk.css"
+  },
+  {
+    "revision": "7d40aee1a912c4b65f3d",
+    "url": "./static/css/2.e4cdbe9c.chunk.css"
+  },
+  {
+    "revision": "19e3de9a1214fce0f3d89ec8b32aeb55",
+    "url": "./index.html"
+  }
+];
